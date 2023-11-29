6b3ff317d43783d65cdf847bcb2de469825a6671
Nicole Vadot <nicole@nvadot.dev>
2023-11-29 11:05:58 +0100

    dumping arr to file

--- main_perf.c
+++ main_perf.c
@@ -15,17 +15,31 @@ long long current_timestamp() {
   return milliseconds;
 }
 
+int write_results(Sim* sim) {
+  FILE *fp = fopen("sim.out", "w");
+  if (!fp) {
+    fprintf(stderr, "unable to open file");
+    return 1;
+  }
+  fprintf(fp, "simulation with nx=%ui, ny=%ui, dt=%f, diff=%f, visc=%f\n", sim->nx, sim->ny, sim->dt, sim->diff, sim->visc);
+  fprintf(fp, "rho\n");
+  writearr(fp, sim->rho, sim->nx*sim->ny);
+  fprintf(fp, "vx\n");
+  writearr(fp, sim->vx, sim->nx*sim->ny);
+  fprintf(fp, "vy\n");
+  writearr(fp, sim->vy, sim->nx*sim->ny);
+  fclose(fp);
+}
+
 int main(int argc, char** argv) {
   const nsteps = 1000;
   uint nx = 100, ny = 100;
 
-  if (argc <= 1) {
-    printf("usage: ./main_perf nx ny\n");
-  }
   if (argc == 3) {
     nx = atoi(argv[1]);
     ny = atoi(argv[2]);
   } else {
+    printf("usage: ./main_perf nx ny\n");
     printf("using default values.\n");
   }
 
@@ -40,14 +54,12 @@ int main(int argc, char** argv) {
     if (istep % 100 == 0) printf("%u / %u\n", istep, nsteps);
 
     // spinny demo
-    for (size_t y = 0; y < sim->ny; ++y) {
-      for (size_t x = 0; x < sim->nx; ++x) {
-        double xf = linterp(x, 0, nx-1, -1, 1), yf = linterp(y, 0, ny-1, -1, 1);
-        double r = sqrt(xf*xf + yf*yf), theta = atan2(yf, xf);
-        sim->vx_prev[IX(x, y)] = 0.01*r*sin(theta);
-        sim->vy_prev[IX(x, y)] = -0.01*r*cos(theta);
-      }
-    }
+    for (size_t y = 0; y < sim->ny; ++y) { for (size_t x = 0; x < sim->nx; ++x) {
+      double xf = linterp(x, 0, nx-1, -1, 1), yf = linterp(y, 0, ny-1, -1, 1);
+      double r = sqrt(xf*xf + yf*yf), theta = atan2(yf, xf);
+      sim->vx_prev[IX(x, y)] = 0.01*r*sin(theta);
+      sim->vy_prev[IX(x, y)] = -0.01*r*cos(theta);
+    }}
     for (size_t i = 0; i < nx*ny; ++i) sim->rho_prev[i] = 0.0;
     sim->rho_prev[IX(nx/2+10,ny/2+10)] = 10.0;
     
@@ -55,6 +67,8 @@ int main(int argc, char** argv) {
   }
 
   printf("finished in %llu milliseconds\n", current_timestamp() - t0);
+
+  write_results(sim);
 
   sim_free(sim);
 
--- utils.h
+++ utils.h
@@ -13,6 +13,14 @@ static int clampi(int x, int a, int b)            { if (x < a) { return a; } els
 __attribute__((always_inline)) inline
 static double clamp(double x, double a, double b) { if (x < a) { return a; } else if (x > b) { return b; } else { return x; } }
 
+static void writearr(FILE* fp, double buf[], uint n) {
+  for (uint i = 0; i < n; ++i) {
+    // todo: a better way to dump the array than just the string
+    fprintf(fp, "%.17f ", buf[i]);
+  }
+  fprintf(fp, "\n");
+}
+
 // static void linspace(double buf[], uint n, double xmin, double xmax) {
 // 	// @brief fills a grid with a constant spacing from xmin to xmax
 // 	double d = (xmax-xmin)/(n-1.0);
